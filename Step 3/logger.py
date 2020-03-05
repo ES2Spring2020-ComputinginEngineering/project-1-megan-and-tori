6f72147a086254931203dfdb417cf4120dcd423a
mhouch01 <60265254+mhouch01@users.noreply.github.com>
2020-03-05 16:59:55 -0500

    check-in

--- Step 3/logger.py
+++ Step 3/logger.py
@@ -1,34 +1,32 @@
 ##################
-# FILL IN HEADER
+# Megan and Tori
 #################
 
 import microbit as mb
 import radio  # Needs to be imported separately
-
 # Change the channel if other microbits are interfering. (Default=7)
 radio.on()  # Turn on radio
-radio.config(channel=7, length=100)
+radio.config(channel=14, length=100)
 
 print('Program Started')
 mb.display.show(mb.Image.HAPPY)
 
-while not mb.button_a.is_pressed():  # wait for button A to be pressed to begin logging
+while not mb.button_b.is_pressed():  # wait for button B to be pressed to begin logging
     mb.sleep(10)
 
+time0 = mb.running_time() #get the current running time
 radio.send('start') # Send the word 'start' to start the receiver
 mb.sleep(1000)
 mb.display.show(mb.Image.HEART)  # Display Heart while logging
 
 
-# Read and send accelerometer data repeatedly until button A is pressed again
-while not mb.button_a.is_pressed():
-    ######################################################
-    # FILL In HERE
+# Read and send accelerometer data repeatedly until button B is pressed again
+while not mb.button_b.is_pressed():
     # Need to collect accelerometer and time measurements
     # Need to format into a single string
     # Send the string over the radio
-    ######################################################
-
+    message = str(mb.running_time()-time0) + ', ' + str(mb.accelerometer.get_x()) + ', ' + str(mb.accelerometer.get_y()) + ', ' + str(mb.accelerometer.get_z())
+    #print(message)
     radio.send(message)
     mb.sleep(10)
 
--- Step 3/receiver.py
+++ Step 3/receiver.py
@@ -1,17 +1,19 @@
 ##################
-# FILL IN HEADER
+# Megan and Tori
 #################
 
 import microbit as mb
 import radio  # Needs to be imported separately
 
+
 # Change the channel if other microbits are interfering. (Default=7)
 radio.on()  # Turn on radio
-radio.config(channel=7, length =100)
+radio.config(channel=14, length =100)
 
 print('Program Started')
 mb.display.show(mb.Image.HAPPY, delay=1000, clear=True)
 
+fout=open('data', 'w')
 
 # Wait for start message before beginning printing
 incoming = ''
@@ -22,14 +24,19 @@ print('start')
 
 while True:
     incoming = radio.receive() # Read from radio
-
+    # Incoming is string sent from logger
+    # Need to parse it and reformat as a tuple for the MU plotter
     if incoming is not None: # message was received
         mb.display.show(mb.Image.HEART, delay=100, clear=True, wait=False)
-        
-        #############################################################
-        # FILL IN HERE
-        # Incoming is string sent from logger
-        # Need to parse it and reformat as a tuple for the MU plotter
-        #############################################################
-
+#         for i in range(3):
+#             incoming = incoming.replace(' ', '')
+        #fout.write(incoming)
+        cut = incoming.split(',')
+        #putting in tuple
+        thing0 = int(cut[0])/1000 #in seconds
+        thing1 = int(cut[1])
+        thing2 = int(cut[2])
+        thing3 = int(cut[3])
+        hold = (thing0, thing1, thing2, thing3)
+        print(hold)
         mb.sleep(10)
